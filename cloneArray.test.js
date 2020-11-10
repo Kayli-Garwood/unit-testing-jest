e45a08a7c770fde50a0eef2565cb3b1b68a2082f
Kayli Garwood <kgarwood@uncc.edu>
2020-11-09 22:50:23 -0500

    Jest Unit Testing Instructions Implemented

---
+++ cloneArray.test.js
@@ -0,0 +1,9 @@
+const { test, expect } = require('@jest/globals')
+const { extractExpectedAssertionsErrors } = require('expect')
+const cloneArray = require('./cloneArray');
+
+test('properly clones array', () => {
+    const array = [1, 2, 3];
+    expect(cloneArray(array)).toEqual(array);
+    expect(cloneArray(array)).not.toBe(array);
+});
